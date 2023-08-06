2896759fbb51c0bdde56db6205a1ff42563ef5f7
Daniel Dytert <daniel.dytert@hanseaticbank.de>
2023-08-06 10:23:12 +0200

    pass csrf token to templates

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"github.com/ddytert/bookings/pkg/config"
 	"github.com/ddytert/bookings/pkg/models"
+	"github.com/justinas/nosurf"
 	"html/template"
 	"log"
 	"net/http"
@@ -17,12 +18,13 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
 // RenderTemplate renders templates using html/template
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 
 	// Get the template cache from app config
 	var tc map[string]*template.Template
@@ -42,7 +44,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 	// render the template
 	buf := new(bytes.Buffer)
 
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 
 	err := t.Execute(buf, td)
 	if err != nil {
